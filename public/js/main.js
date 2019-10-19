970140d6dec6450b6175f48afbfcdf9910516f1c
Tkfm21 <tkfm21@gmail.com>
2019-10-19 12:56:17 +0900

    web create Api add

--- public/js/main.js
+++ public/js/main.js
@@ -2,16 +2,25 @@
     const todos = [];
     const KEYS = ['id', 'title', 'body', 'createdAt', 'updatedAt'];
     const tbody = document.getElementById('tbody');
+    const addTitle = document.getElementById('add-title');
+    const addBody = document.getElementById('add-body');
+    const addBtn = document.getElementById('add-btn');
     const API_URL_GET_ALL = 'http://localhost:3001/api';
 
-    window.addEventListener('load', async (event) => {
-        const res = await fetch(API_URL_GET_ALL);
-        const dataArray = await res.json();
-        dataArray.forEach(data => {
-            todos.push(data);
-        });
-        
-        todos.forEach(todo => {
+    window.addEventListener('load', (event) => {
+        listTodos();
+    });
+
+    const allTodosDel = () => {
+        while(tbody.firstChild) tbody.removeChild(tbody.firstChild);
+    };
+
+    const listTodos = async () => {
+        allTodosDel();
+        const dataArray = await getTodosApi();
+        dataArray.forEach(todo => {
+            todos.push(todo);
+
             const trEle = document.createElement('tr');
             KEYS.forEach(key => {
                 const tdEle = document.createElement('td');
@@ -20,18 +29,58 @@
             });
             const delBtn = document.createElement('button');
             delBtn.textContent = 'Delete';
-            delBtn.addEventListener('click', async (event) => {
-                const trEle = event.target.parentNode;
+            delBtn.addEventListener('click', (event) => {
+                const tdEle = event.target.parentNode;
+                const trEle = tdEle.parentNode;
                 const id = trEle.firstElementChild.textContent;
-                while (trEle.firstChild) {
-                    trEle.removeChild(trEle.firstChild);
-                }
-                const response = await fetch(`http://localhost:3001/api/${id}`, {method: 'DELETE'});
-                const deleteTodo = await response.json();
-                console.log(deleteTodo);
+                todoDelApi(id);
+                listTodos();
             });
-            trEle.appendChild(delBtn);
+            const tdEle = document.createElement('td');
+            tdEle.appendChild(delBtn);
+            trEle.appendChild(tdEle);
             tbody.appendChild(trEle);
         });
+    };
+
+    const getTodosApi = async () => {
+        const todos =  await fetch(API_URL_GET_ALL)
+                                .then(res => res.json())
+                                .catch(error => {
+                                    alert(error.message);
+                                });
+        return todos;
+    };
+
+    const todoDelApi = async (id) => {
+        await fetch(`http://localhost:3001/api/${id}`, {method: 'DELETE'})
+                .then(res => res.json())
+                .catch(error => {
+                    alert(error.message);
+                });
+    };
+
+    addBtn.addEventListener('click', async (event) => {
+        const addTitleText = addTitle.value;
+        addTitle.value = '';
+        const addBodyText = addBody.value;
+        addBody.value = '';
+
+        if (addTitleText && addBodyText) {
+            console.log(addTitleText, addBodyText, 'no empty');
+            const addData = {title: addTitleText, body: addBodyText};
+            const addTodo = await fetch(API_URL_GET_ALL, {
+                                            method: 'POST',
+                                            headers: {"Content-Type": "application/json; charset=utf-8"},
+                                            body: JSON.stringify(addData)
+                                        })
+                                    .then(res => res.json())
+                                    .catch(error => {
+                                        alert(error.message);
+                                    });
+            listTodos();
+        } else {
+            alert('入力値が不正です');
+        }
     });
 })();
